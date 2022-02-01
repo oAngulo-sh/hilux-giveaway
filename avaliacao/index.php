7654b6ce981d67bbd89ee03d837d7ea2818e1f14
Your Name <bernardosr@hotmail.com.br>
2022-02-01 10:32:32 -0300

    estrutura de pastas

---
+++ avaliacao/index.php
@@ -0,0 +1,2 @@
+<?php
+header('location: ./screens/menu/menu.php');
---
+++ avaliacao/screens/menu/menu.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="UTF-8">
+  <link href="styles/styles.css" rel="stylesheet" type="text/css">
+  <title>Menu</title>
+</head>
+
+<body>
+  <div class="header">
+    <h1>Avaliação de PHP</h1>
+  </div>
+</body>
+
+</html>
